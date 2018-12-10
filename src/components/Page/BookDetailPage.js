1e9f781f7f2935640ee53a2294365fea230633ad
beyazsu <beyazsu001@gmail.com>
2018-12-10 22:51:38 +0300

    App'ten gelen state Bookdetail e props olarak geçirildi

--- src/components/Page/BookDetailPage.js
+++ src/components/Page/BookDetailPage.js
@@ -3,13 +3,21 @@ import {PropTypes} from 'prop-types';
 import BookDetail from '../BookDetail';
 
 class BookDetailPage extends Component{
+   
 
     render(){
+        const book= this.props.books.find(item=>item.kitap_no===this.props.match.params.id);
+        console.log(book)
+        console.log(this.props.books)
         return(
             <div>
                 BookDetailPage --> BookDetail
+                <BookDetail 
+                books={this.props.books}
+                book={book} />
             </div>
         )
     }
 }
+
 export default BookDetailPage;

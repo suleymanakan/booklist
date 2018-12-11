c89b7c41ce6de17b1af50c4f9ccf088e5ae6ef33
beyazsu <beyazsu001@gmail.com>
2018-12-12 00:37:23 +0300

    Kitap gösterimi için kulanılacak Card eklendi

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Card, Grid } from 'semantic-ui-react'
 class BooksList extends React.Component{
 
 
     render(){
         return(
-            <div><hr/>
-                {this.props.books.map(book=>
-                <div>
-                    <h5>{book.kitap_ad}</h5>
-                    <h4>{book.kitap_yayin}</h4><hr/> </div>)}
+            <div>
+            <Grid  columns={3}>
+               { this.props.books.map((book,key)=>(
+                   <Grid.Column key={key}>
+                       <Card 
+                         image={book.kitap_resim_url}
+                         header={book.kitap_ad}/>
+                   </Grid.Column>
+
+
+                ))}
+            </Grid>
             </div>
         )
     }

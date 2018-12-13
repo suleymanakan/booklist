f2e9303c01736561b8731a803eff5c88f8b561ab
beyazsu <beyazsu001@gmail.com>
2018-12-13 23:03:03 +0300

    Kitap arama inputu oluşturuldu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React,{Component} from 'react';
+import {PropTypes} from 'prop-types';
+import '../App.css';
 import 'semantic-ui-css/semantic.min.css';
 import {Container,Image,Menu,Visibility} from 'semantic-ui-react';
 
@@ -20,13 +22,29 @@ const menuStyle = {
 class Header extends Component{
     state = {
         menuFixed: false,
+        filterText: ''
       
+    }
+    static propType={
+        books: PropTypes.array.isRequired
     }
     stickTopMenu = () => this.setState({ menuFixed: true })
     unStickTopMenu = () => this.setState({ menuFixed: false })
+    
+    onChangeFilterText =(e)=>{
+        this.setState({
+            filterText: e.target.value
+        })
+    }
 
     render(){
-        const { menuFixed } = this.state
+        const { menuFixed } = this.state;
+        /*const book = this.props.books.filter(
+            books=>{
+                return books.kitap_ad.toLowerCase().indexOf(
+                    this.state.filterText.toLowerCase()
+                ) !== -1 }
+        );*/
         return(
             <div>
                 <Visibility
@@ -46,11 +64,25 @@ class Header extends Component{
                             <Menu.Item as='a'>Kitap Ekle</Menu.Item>
 
                             <Menu.Item position='right'>
-                        
+                            <input 
+                            value={this.state.filterText}
+                            onChange={this.onChangeFilterText}
+                            id='filter'
+                            name='filter'
+                            placeholder='Kitap adı veya yayın adı arayın'     />
                             </Menu.Item>
                             </Container>
                     </Menu>
+                    <Container >
+                    <Menu position='right' width='100px' >
+                        {/*book.map(book=>
+                            <Menu.Item position='right'>{book.kitap_ad}</Menu.Item>)*/}
+                    </Menu>
+                    </Container>
+                    
+                    
                 </Visibility>
+
             </div>
         )
     }

104fd4502d75ec0a16e77d123e0672cdbd660639
beyazsu <beyazsu001@gmail.com>
2018-12-13 23:04:32 +0300

    Kitap Cardlarına detay buttonu eklendi

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import {Grid,Card} from 'semantic-ui-react'
+import {Grid,Card, Button,Icon} from 'semantic-ui-react'
 
+
+const extra=(
+    <div>
+        <Button animated >
+        <Button.Content visible>Detay</Button.Content>
+        <Button.Content hidden>
+            <Icon name='arrow right' />
+        </Button.Content>
+        </Button>
+    </div>
+)
 class BooksList extends React.Component{
 
     render(){
@@ -11,7 +22,8 @@ class BooksList extends React.Component{
                     <Grid.Column key={key}>
                         <Card 
                         image={book.kitap_resim_url}
-                        header={book.kitap_ad} />
+                        header={book.kitap_ad}
+                        extra={extra} />
                     </Grid.Column>
                     ))}
                 </Grid>
